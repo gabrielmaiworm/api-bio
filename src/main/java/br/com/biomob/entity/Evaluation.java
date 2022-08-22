7434f3b8d1b7250012088083b3c753ab8a4a16e4
unknown <tuxomaiworm@hotmail.com>
2022-08-22 09:47:58 -0300

    update

--- src/main/java/br/com/biomob/entity/Evaluation.java
+++ src/main/java/br/com/biomob/entity/Evaluation.java
@@ -73,7 +73,6 @@ public class Evaluation {
 	@Column(name = "audio_menu")
 	private Boolean audioMenu; //cardapio com audio - Resultados possiveis: true | false
 	
-	
 	@Column(name = "star")
 	private Double star; //nota
 	
---
+++ src/main/java/br/com/biomob/interfaces/IEvaluationService.java
@@ -0,0 +1,7 @@
+package br.com.biomob.interfaces;
+
+public interface IEvaluationService {
+	
+	
+
+}
