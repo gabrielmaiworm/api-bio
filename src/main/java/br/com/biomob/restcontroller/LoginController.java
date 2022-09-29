4b1ee83c052f407bff4c539a0a5502d4a237e4e8
gabriel <tuxomaiworm@hotmail.com>
2022-09-29 15:09:52 -0300

    update

--- src/main/java/br/com/biomob/restcontroller/LoginController.java
+++ src/main/java/br/com/biomob/restcontroller/LoginController.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,9 @@ public class LoginController {
 	public ResponseEntity<List<User>> listarTodos() {
 		return ResponseEntity.ok(userRepository.findAll());
 	}
-		
+	
+	@DeleteMapping("/user/{email}")
+	public void removeUser(@PathVariable String email) {
+		userRepository.deleteById(email);
+	}	
 }
